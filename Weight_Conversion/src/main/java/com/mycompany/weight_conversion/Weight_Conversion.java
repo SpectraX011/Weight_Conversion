76f9ac140e26fd374fd3bb55aa35d229d3c6440f
Daniel S. Prudenciado <218104746+SpectraX011@users.noreply.github.com>
2026-02-04 21:06:08 +0800

    Add files via upload

---
+++ Weight_Conversion/src/main/java/com/mycompany/weight_conversion/Weight_Conversion.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.weight_conversion;
+
+/**
+ *
+ * @author Nullroot 13
+ */
+/*
+import java.util.Scanner;
+public class Weight_Conversion {
+
+    public static void main(String[] args) {
+        // weight conversiom program
+        // declare varibales
+        //welcome message
+        //prompt for user choice
+        //option 1 convert lbs to kgs
+        //option 2 convert kgs to lbs 
+        //else print not valid choice
+        
+        Scanner sc = new Scanner(System.in);
+        
+        double weight;
+        double newWeight;
+        int choice;
+        
+        System.out.println("Weight Conversion Program");
+        System.out.println("1: Covert to lbs to kgs");
+        System.out.println("2:Convert to kgs to lbs");
+        
+        System.out.println("Choose an option:  ");
+        choice = sc.nextInt();
+        
+        
+        if(choice == 1){
+            System.out.println("Enter the weight in lbs");
+            weight = sc.nextDouble();
+            newWeight = weight * 0.453592;
+            
+            System.out.println("The new weight in kgs is:" + newWeight);
+        }
+        
+      else  if(choice == 2){
+            System.out.println("Enter the weight in kgs: ");
+            weight = sc.nextDouble();
+            newWeight = weight * 2.20462;
+            
+            System.out.printf("The new weight in kgs is: %.2f",newWeight);
+        }
+        
+      else{
+            System.out.println("That was not a valid choice");
+      }
+        
+        sc.close();
+        
+        
+        
+        
+        
+    }
+}
+
+
+*/
+
+
